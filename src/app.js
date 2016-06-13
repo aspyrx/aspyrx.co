9361b4859f5f68563c8f7adb9f9ec974acd19b43
Stan Zhang <stan.zhang2@gmail.com>
2016-06-13 03:26:51 -0400

    Basic logo/header

--- src/app.js
+++ src/app.js
@@ -3,8 +3,31 @@
  */
 
 import React, {Component} from 'react';
+import {Link} from 'react-router';
 
 import styles from 'app.less'
+
+function Logo() {
+    return <div className={styles.logo}>
+        <div className={styles.right}>
+            <div className={styles.left}>
+                <div className={styles.middle} />
+            </div>
+        </div>
+    </div>;
+}
+
+function Header() {
+    return <div className={styles.header}>
+        <Logo />
+        <div className={styles.logoText}>
+            <span>SPYRX.CO</span>
+        </div>
+        <div className={styles.navigation}>
+            <Link to="/" activeClassName="active">home</Link>
+        </div>
+    </div>;
+}
 
 export default class App extends Component {
     static get propTypes() {
@@ -16,6 +39,7 @@ export default class App extends Component {
     render() {
         return (
             <div className={styles.container}>
+                <Header />
                 {this.props.children}
             </div>
         );
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,13 @@
 
 import React, {Component} from 'react';
 
+import styles from './index.less';
+
 export default class Index extends Component {
     render() {
         return (
-            <h1>Hello React!</h1>
+            <div className={styles.container}>
+            </div>
         );
     }
 }
