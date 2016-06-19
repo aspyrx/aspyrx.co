dd6f9077964c40c1249e9b011a609e1e3cad37bd
Stan Zhang <stan.zhang2@gmail.com>
2016-06-18 23:52:01 -0400

    Added page change transitions

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactCSSTransitionReplace from 'react-css-transition-replace';
 import Header from './components/header';
 
 import styles from 'app.less'
@@ -9,7 +10,8 @@ import bg from './images/bg.svg';
 export default class App extends Component {
     static get propTypes() {
         return {
-            children: React.PropTypes.node
+            children: React.PropTypes.node,
+            location: React.PropTypes.object
         };
     }
 
@@ -22,13 +24,32 @@ export default class App extends Component {
     }
 
     render() {
+        const { location: { pathname }, children } = this.props;
+
         return <div className={styles.containers}>
             <object className={styles.bg} data={bg} type="image/svg+xml" />
             <div className={styles.container}>
                 <Header />
             </div>
             <div className={styles.container}>
-                {this.props.children}
+                <ReactCSSTransitionReplace transitionName={{
+                    enter: styles.enterInc,
+                    enterActive: styles.enterIncActive,
+                    leave: styles.leaveInc,
+                    leaveActive: styles.leaveIncActive,
+                    appear: styles.appearInc,
+                    appearActive: styles.appearIncActive
+                }}
+                transitionAppear={true}
+                transitionAppearTimeout={300}
+                transitionEnterTimeout={600}
+                transitionLeaveTimeout={300}
+                overflowHidden={false}>
+                {children
+                    ? React.cloneElement(children, { key: pathname })
+                    : null
+                }
+                </ReactCSSTransitionReplace>
             </div>
         </div>;
     }
